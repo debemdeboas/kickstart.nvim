954423966345dc13f2005affae10328ba848e6e5
Rafael de Bem <debemrafael@gmail.com>
2024-04-25 18:38:58 -0300

    remove cursor from neotree and add setup script

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -115,6 +115,20 @@ return {
     },
     config = function()
       require('neo-tree').setup {
+        event_handlers = {
+          {
+            event = 'neo_tree_buffer_enter',
+            handler = function()
+              vim.cmd 'highlight! Cursor blend=100'
+            end,
+          },
+          {
+            event = 'neo_tree_buffer_leave',
+            handler = function()
+              vim.cmd 'highlight! Cursor guibg=#5f87af blend=0'
+            end,
+          },
+        },
         source_selector = {
           winbar = true,
         },
---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v nvim)"  ]; then
+	echo "Installing NeoVim..."
+	curl -LO "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
+	sudo rm -rf /opt/nvim
+	sudo tar -C /opt -xzf nvim-linux64.tar.gz
+	echo "export PATH=\"$PATH:/opt/nvim-linux64/bin\"" >> ~/.bashrc
+fi
+
+if ! [ -x "$(command -v rg)" ]; then
+	echo "Installing ripgrep..."
+
+	rgver=$(curl -I -s "https://github.com/BurntSushi/ripgrep/releases/latest/" | grep location: | awk '{print substr($2,52)}')
+	curl -LO "https://github.com/BurntSushi/ripgrep/releases/tag/${rgver}/ripgrep_${rgver}_amd64.deb"
+	sudo dpkg -i ripgrep_${rgver}_amd64.deb
+
+	sudo apt update && sudo apt install -y ripgrep
+fi
+
